ce431d58430e9bf5d647f3dd61e60793ca1b6c04
Manmath Mahajan <manmathmahajan@Manmaths-MacBook-Air.local>
2025-07-08 05:21:39 +0530

    Remove all files from repository

--- auth_example.py
+++
@@ -1,5 +0,0 @@
-# Example auth.py
-# Copy this file to auth.py and fill in your own credentials
-USERNAME = "your_username"
-PASSWORD = "your_password"
-# Add other required authentication variables below
